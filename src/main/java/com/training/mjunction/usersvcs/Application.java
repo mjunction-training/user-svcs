a9cb0b7026a5b3821b2397633a99ceeac183a10c
talus005 <expogrow.org@gmail.com>
2019-02-10 23:31:29 +0530

    Initial commit

--- src/main/java/com/training/mjunction/usersvcs/Application.java
+++ src/main/java/com/training/mjunction/usersvcs/Application.java
@@ -4,19 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
 		return application.sources(Application.class);
 	}
 
-
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-}
+	@Bean
+	public WebMvcConfigurer CORSConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(final CorsRegistry registry) {
+				registry.addMapping("/api/v1/**");
+			}
+		};
+	}
 
+}
