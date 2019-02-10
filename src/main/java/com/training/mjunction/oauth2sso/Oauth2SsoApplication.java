2a7d0320355a8d5acffd714c06b736eb15b57f6e
talus005 <expogrow.org@gmail.com>
2019-02-10 20:32:08 +0530

    Initial commit

---
+++ src/main/java/com/training/mjunction/oauth2sso/Oauth2SsoApplication.java
@@ -0,0 +1,14 @@
+package com.training.mjunction.oauth2sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SsoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/training/mjunction/oauth2sso/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.training.mjunction.oauth2sso;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Oauth2SsoApplication.class);
+	}
+
+}
+
