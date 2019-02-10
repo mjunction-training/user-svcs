a4dabe74c535d8b170e6f122f5b12f80f5b2756c
talus005 <expogrow.org@gmail.com>
2019-02-11 00:27:52 +0530

    Initial Commit

--- src/test/java/com/training/mjunction/usersvcs/ApplicationTests.java
+++ src/test/java/com/training/mjunction/usersvcs/ApplicationTests.java
@@ -1,17 +1,40 @@
 package com.training.mjunction.usersvcs;
 
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import io.restassured.RestAssured;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
 
+	@LocalServerPort
+	int port;
+
+	@Before
+	public void setUp() {
+		RestAssured.port = port;
+	}
+
 	@Test
-	public void contextLoads() {
+	public void infoTest() {
+		RestAssured.given().when().contentType("application/json").get("/actuator/info").then().statusCode(200).body("app.name",
+				containsString("user-svcs"));
+	}
+
+	@Test
+	public void healthTest() {
+		RestAssured.given().when().contentType("application/json").get("/actuator/health").then().statusCode(200).body("status",
+				containsString("UP"));
 	}
 
 }
-
