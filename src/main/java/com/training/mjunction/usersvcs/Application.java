6f56af16a2a710f4b8841414e2d07e773ed94223
talus005 <expogrow.org@gmail.com>
2019-02-10 20:48:15 +0530

    Initial commit

--- src/main/java/com/training/mjunction/usersvcs/Application.java
+++ src/main/java/com/training/mjunction/usersvcs/Application.java
@@ -1,13 +1,21 @@
-package com.training.mjunction.oauth2sso;
+package com.training.mjunction.usersvcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Oauth2SsoApplication {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(Oauth2SsoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/training/mjunction/usersvcs/ApplicationTests.java
+++ src/test/java/com/training/mjunction/usersvcs/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.training.mjunction.oauth2sso;
+package com.training.mjunction.usersvcs;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Oauth2SsoApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
